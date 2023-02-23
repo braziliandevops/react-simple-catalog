0040248ebc1749d9e43e8fab7716899829c608b0
Michel Costa <lima.mishell@gmail.com>
2023-02-23 03:58:11 -0300

    generic elements on the new page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,9 +1,18 @@
+import Banner from 'components/Banner'
+import Card from 'components/Card'
+import Title from 'components/Title'
 import style from './Favorites.module.css'
 
 function Favorites() {
     return (
         <>
-        <h1>Ok</h1>
+            <Banner image="favorites" />
+            <Title>
+                <h1>Favorites</h1>
+            </Title>
+            <section className={style.container}>
+                <Card id="1" key="1" title="New title" cover="https://cdn.marvel.com/content/1x/spidermannwh_hardcover.jpg" />
+            </section>
         </>
     )
 }
