67355296d0383207ea2c98e91552e3d202a2362c
Michel Costa <lima.mishell@gmail.com>
2023-02-23 03:03:04 -0300

    dynamic list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import Card from 'components/Card'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import Title from 'components/Title'
+import videos from 'json/db.json'
+import style from './Home.module.css'
 
 function Home() {
     return (
@@ -12,7 +14,11 @@ function Home() {
             <Title>
                 <h1>Title component</h1>
             </Title>
-            <Card id='1' title='Movie 1' cover='https://m.media-amazon.com/images/I/71x1RHSaEhL.jpg' />
+            <section className={style.container}>
+                {videos.map( video => {
+                    return <Card {... video} key={video.id} />
+                })}
+            </section>
             <Footer />
         </>
     )
