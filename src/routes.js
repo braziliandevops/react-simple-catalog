71e05a38b744c9737516b4a3f492dae9435f680c
Michel Costa <lima.mishell@gmail.com>
2023-02-24 22:34:40 -0300

    context and custom hook

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import Container from "components/Container";
 import Favorites from "pages/Favorites";
+import FavoritesProvider from "contexts/Favorites";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Header />
             <Container>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/favorites" element={<Favorites />}></Route>
-                </Routes>
+                <FavoritesProvider>
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/favorites" element={<Favorites />}></Route>
+                    </Routes>
+                </FavoritesProvider>
             </Container>
             <Footer />
         </BrowserRouter>
